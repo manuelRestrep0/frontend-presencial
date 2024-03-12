0caae91367af93a4f388370b3812d1a0ac181982
Eliana Puerta <80785262+Eliana-Janneth@users.noreply.github.com>
2024-03-12 10:31:16 -0500

    feat: first commit

---
+++ app/modulo-pagos-a/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Web() {
+  return (
+    <div className="text-center text-4xl text-violet-800 font-bold ">
+      ¡Hola Mundo desde el módulo de pagos A con ID!
+    </div>
+  )
+}
