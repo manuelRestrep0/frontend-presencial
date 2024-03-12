4119e906f37c913dd756d3d7592f6a58c8aff6ac
Alejandro-XIII <diegoa.castaneda@udea.edu.co>
2024-03-12 09:38:46 -0500

    Hola mundo

---
+++ app/gestion_vuelosB/page.tsx
@@ -0,0 +1,7 @@
+export default function Web() {
+    return (
+         <div style={{ textAlign: 'center', fontSize: '3em', fontWeight: 'bold', color: '#ff5733', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+            ¡Hola Mundo!
+        </div>
+    )
+  }
