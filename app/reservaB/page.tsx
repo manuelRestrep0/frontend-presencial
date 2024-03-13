5478282f3946f0c0381bbe46aa9e52aef1bee670
Maira Rodas <maira.rodas@udea.edu.co>
2024-03-13 13:13:32 -0500

    First Deploy

--- app/reservaB/page.tsx
+++ app/reservaB/page.tsx
@@ -1,7 +1,7 @@
 export default function Web() {
     return (
         <div className="flex justify-center items-center h-screen w-full">
-            Hola Mundo, desde Reserva B.
+            Hola Mundo, saluda Reserva B.
         </div>
     )
 }
