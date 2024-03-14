c3852804ade4e436718fd54fc1bb96973fb57ac5
Jose Waldo <josferw@gmail.com>
2024-03-14 16:49:30 -0500

    Feat: se añadió la página inicial para el módulo

---
+++ app/gestion-asientos/page.tsx
@@ -0,0 +1,7 @@
+export default function GestionAsientos() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">Módulo Gestión de Asientos</h1>
+    </main>
+  )
+}
