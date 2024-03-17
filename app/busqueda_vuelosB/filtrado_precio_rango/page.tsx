a1ca13708b14ad67b51b70023d6660066e570047
vasquezEmanuel <emanuel.vasquez@udea.edu.co>
2024-03-17 08:26:30 -0500

    Agregado archivo de página de filtro de precios y rango de horas

---
+++ app/busqueda_vuelosB/filtrado_precio_rango/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FiltroPrecioRango() {
+    return (
+        <div>
+            <h1>Filtro de búsqueda de vuelos por precio y rango de horas</h1>
+            <form>
+                {/* Aquí colocarías tus elementos de entrada de datos, como campos de texto o controles deslizantes */}
+            </form>
+        </div>
+    );
+}
+
+export default FiltroPrecioRango;
