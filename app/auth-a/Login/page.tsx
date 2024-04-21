01955561b534aa9d467189ec7fb5783d1b3bf5dc
Juanpablo733 <bedoya.pablo.7@gmail.com>
2024-04-21 17:09:49 -0500

    resolver conflictos

--- app/auth-a/Login/page.tsx
+++ app/auth-a/Login/page.tsx
@@ -8,6 +8,9 @@ import { BsChevronLeft } from 'react-icons/bs';
 import { LiaPlaneDepartureSolid } from "react-icons/lia";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
+import Link from 'next/link';
+import { IconAirplane } from 'components/components-Auth-a/IconAirplane';
+import { InputField } from 'components/components-Auth-a/InputField';
 
 
 const Form = () => {
@@ -16,30 +19,30 @@ const Form = () => {
     const [windowWidth, setWindowWidth] = useState(0);
 
     useEffect(() => {
-    
-      setWindowWidth(document.documentElement.clientWidth);
-    
-      const handleResize = () => {
-        setWindowWidth(document.documentElement.clientWidth);
-      };
-    
-      window.addEventListener("resize", handleResize);
-    
 
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+        setWindowWidth(document.documentElement.clientWidth);
+
+        const handleResize = () => {
+            setWindowWidth(document.documentElement.clientWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
     }, []);
 
     interface FormData {
-        email: string; 
+        email: string;
         password: string;
         rememberMe: boolean;
     }
 
     const [formData, setFormData] = useState({
-        email:"",
-        password:"",
+        email: "",
+        password: "",
         rememberMe: false,
     });
 
@@ -65,7 +68,7 @@ const Form = () => {
                 setPage(page - 2)
             }
         } else {
-    
+
             if (page === 0) {
                 alert("hallo, you're at the start")
             } else {
@@ -75,94 +78,67 @@ const Form = () => {
         }
     }
 
-    
+
     return (
         <div className='main-container h-screen bg-cover text-black flex justify-center items-center'>
 
-            <div id='login-container' className='form-container bg-white h-[65%] w-[70%] rounded-2xl relative'>
-
-                <div id='login_header' className='h-[15%] inset-y-0 left-0 w-[60%] bg-white flex rounded-l-2xl'> 
-                    <button id='header-back-btn' onClick={prevPage} className='flex justify-center items-center gap-2'>
-                        <BsChevronLeft className='ml-4'/>
-                        <h2 className='-mt-1'>Inicio de sesión</h2>
-                    </button>
-                    
-                </div>
-                
-                <div className='h-[3%] inset-y-0 left-0 w-[60%]  bg-white flex place-items-start'>
-                    <h2 className='text-xs ml-5 -mt-6 mb-4'>¿Eres usuario nuevo?</h2>
-                    <button className='text-xs text-otro-azul ml-2 -mt-6 mb-1'>Regístrate aquí.</button>
-                </div>
-    
-                <div id='login_body ' className='form-container inset-y-0 left-0 w-[60%] bg-white flex place-items-start flex-col'>
-                    
-                    <form onSubmit={handleSubmit} className='form-container inset-y-0 left-0 bg-white flex-col flex w-[100%] '>
-                        <div className="mb-4 ml-5 flex flex-col w-[60%]">
-                            <label htmlFor="email">Correo</label>
-                            <input 
-                                type="email" 
-                                id="email" 
-                                name="email" 
-                                value={formData.email} 
-                                onChange={handleChange} 
-                                required 
-                                className="border-b border-gray-600 p-1 mb-1 "
-                            />
+            <div className='flex bg-white h-[511px] w-[525px] rounded-2xl relative'>
+                <div className='w-3/5 flex flex-col p-8 gap-4'>
+                    <div className=' flex flex-col gap-2 w-52 items-start'>
+                        <button onClick={prevPage} className='flex w-full text-xl items-center justify-between'>
+                            <BsChevronLeft />
+                            <h2 className=''>Inicio de sesión</h2>
+                        </button>
+                        <div className='flex w-full justify-between items-center'>
+                            <h2 className='text-[10px]'>¿Eres usuario nuevo?</h2>
+                            <button className='text-[10px] text-[#0077B6]'>
+                                <Link href={"signup"}>Regístrate aquí.</Link>
+                            </button>
                         </div>
-                        <div className="mb-4 ml-5 flex flex-col w-[60%]">
-                            <label htmlFor="password">Contraseña</label>
-                            
-                            <input 
-                                type="password" 
-                                id="password" 
-                                name="password" 
-                                value={formData.password} 
-                                onChange={handleChange} 
-                                required 
-                                className="border-b border-gray-600 p-1 mb-1 "
-                                
-                            />
-                        </div>
-                            
-                        <div className='h-[3%] inset-y-0 left-0 w-[60%]  bg-white flex place-items-start items-center'>
-                            <button className='text-xxs text-otro-azul ml-5 -mt-3 mb-2 '>¿Ha olvidado su contraseña?</button>
-                        </div>
+                    </div>
 
-                        <div id='remember' className="ml-5 flex items-baseline justify-left text-center flex-row w-[85%]">
-                            
-                            <input 
-                                type="checkbox" 
-                                id="rememberMe" 
-                                name="rememberMe" 
-                                checked={formData.rememberMe} 
-                                onChange={handleChange} 
-                            />
-                            <label htmlFor="rememberMe" className="ml-2 mb-3">Recuérdame</label>
-                            <button className='nav-btn w-20 h-7 flex items-center justify-center text-center ml-[auto] mb-3 ' >Ingresar</button>
-                        </div>
 
-                    </form>
-                </div>
-                    
-                <hr className="border-gray-600 inset-y-0 left-0 w-[55%] mx-6 justify-center mb-5" /> 
-    
-                <div className='h-[15%] inset-y-0 left-0 w-[60%] bg-white place-items-start flex flex-col items-center justify-center space-y-4 mb-5'>
-                    <button className='log-btn w-[80%] h-8 flex items-center justify-center text-center gap-3'>
-                        <FcGoogle className='text-xl'/>Ingresar con Google
-                    </button>
-                    <button className='log-btn w-[80%] h-8 flex items-center justify-center text-center gap-3'>
-                        <BsFacebook className='text-facebook-logo text-xl'/>Ingresar con Facebook
-                    </button>
-                </div>
+                    <div className=' w-full bg-white flex flex-col'>
+                        <form onSubmit={handleSubmit} className=' bg-white flex-col flex gap-4 mt-3'>
+                            <InputField id="email" placeholder="Usuario" onChange={handleChange} type='email' value={formData.email} />
+                            <InputField id="password" placeholder="Contraseña" onChange={handleChange} type='password' value={formData.password} />
+                            <button className=' text-xs text-gray-600'>¿Ha olvidado su contraseña?</button>
+                            <div className=" flex justify-between items-center">
+                                <div className='flex gap-4'>
+                                    <input
+                                        type="checkbox"
+                                        id="rememberMe"
+                                        name="rememberMe"
+                                        checked={formData.rememberMe}
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="rememberMe" className=" text-xs">Recuérdame</label>
+                                </div>
+                                <button className='nav-btn w-20 h-7 flex items-center justify-center text-center ' >Ingresar</button>
+                            </div>
 
-                <div>
-                    <button className='text-xs text-otro-azul -mt-6 ml-5 '>Obtener ayuda</button>
-                </div>
+                        </form>
+                    </div>
 
-    
-                <div className="absolute inset-y-0 right-0 w-[40%] bg-login-blue rounded-r-2xl flex justify-center items-center ">
+                    <hr className="border-gray-600 w-full justify-center mt-7" />
+
+                    <div className=' mt-7 bg-white w-full flex flex-col items-center gap-2 justify-center'>
+                        <button className=' border border-gray-600 rounded-full text-xs w-full h-8 flex items-center justify-center text-center gap-3'>
+                            <FcGoogle className='text-xl' />Ingresar con Google
+                        </button>
+                        <button className='border border-gray-600 rounded-full text-xs w-full h-8 flex items-center justify-center text-center gap-3'>
+                            <BsFacebook className='text-facebook-logo text-xl' />Ingresar con Facebook
+                        </button>
+                    </div>
+
                     <div>
-                        <LiaPlaneDepartureSolid className='text-avion-blue text-8xl' />
+                        <button className='text-xs mt-7 text-[#0077B6]'>Obtener ayuda</button>
+                    </div>
+                </div>
+
+                <div className="absolute inset-y-0 right-0 w-[40%] bg-[#219EBC] rounded-r-2xl flex justify-center items-center ">
+                    <div>
+                        <IconAirplane />
                         <h2 className='text-white flex justify-center items-center font-bold'>SITAS</h2>
                         <h3 className='text-white text-sm flex justify-center items-center'>Singapur Airlines</h3>
                     </div>
@@ -170,6 +146,6 @@ const Form = () => {
             </div>
         </div>
     );
-}  
+}
 
 export default Form;
--- app/auth-a/page.tsx
+++ app/auth-a/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link"
+
 export default function Web() {
     return (
-        <div className="flex justify-center items-center h-screen w-full">
+        <div className="flex flex-col gap-6 justify-center items-center h-screen w-full">
             Hola Mundo, desde autenticación y autorización A...
+            <button className="bg-slate-400 p-5">
+
+                <Link href={"auth-a/Login"}>Ir a Auth-a Login</Link>
+            </button>
         </div>
     )
 }
