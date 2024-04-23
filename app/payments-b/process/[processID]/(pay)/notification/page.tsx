8817e74cf425493d05511d7703ef3b7a540654b6
David Upeugigit config --local user.name David <dupegui2002@gmail.com>
2024-04-22 23:05:27 -0500

    Showing a loading state in the notification fetch

--- app/payments-b/process/[processID]/(pay)/notification/page.tsx
+++ app/payments-b/process/[processID]/(pay)/notification/page.tsx
@@ -7,32 +7,27 @@ import remove from "@/../public/remove.png"
 import { useEffect, useState } from "react"
 
 interface CostsData {
-  seatCost: number;
-  flightCost: number;
-  baggageCost: number;
-  otherCosts: number;
-  totalCost: number;
+  seatCost: number
+  flightCost: number
+  baggageCost: number
+  otherCosts: number
+  totalCost: number
 }
 
-export default function Notification({params}: {params: {processID: string}}) {
-  //Respuesta asociada al estado de la transacción: true -> aceptada, false -> rechazada
+export default function Notification({ params }: { params: { processID: string } }) {
   const getCurrentDate = () => {
-    // Obtiene la fecha y hora actual
-    const fechaHoraActual = new Date();
-    
-    // Obtiene los componentes de la fecha y hora actual
-    const dia = String(fechaHoraActual.getDate()).padStart(2, '0');
-    const mes = String(fechaHoraActual.getMonth() + 1).padStart(2, '0'); // El mes es devuelto indexado desde 0
-    const año = fechaHoraActual.getFullYear();
-    let horas = fechaHoraActual.getHours();
-    const minutos = String(fechaHoraActual.getMinutes()).padStart(2, '0');
-    
-    // Formatea las horas al formato de 12 horas y determina si es AM o PM
-    const am_pm = horas >= 12 ? 'PM' : 'AM';
-    horas = horas % 12 || 12; // Si son las 0:00, cambia a 12:00 AM
-    
-    // Retorna la fecha y hora formateada
-    return `${dia}/${mes}/${año} ${horas}:${minutos} ${am_pm}`;
+    const fechaHoraActual = new Date()
+
+    const dia = String(fechaHoraActual.getDate()).padStart(2, "0")
+    const mes = String(fechaHoraActual.getMonth() + 1).padStart(2, "0") // El mes es devuelto indexado desde 0
+    const año = fechaHoraActual.getFullYear()
+    let horas = fechaHoraActual.getHours()
+    const minutos = String(fechaHoraActual.getMinutes()).padStart(2, "0")
+
+    const am_pm = horas >= 12 ? "PM" : "AM"
+    horas = horas % 12 || 12 // Si son las 0:00, cambia a 12:00 AM
+
+    return `${dia}/${mes}/${año} ${horas}:${minutos} ${am_pm}`
   }
 
   const defaultCostsData: CostsData = {
@@ -40,20 +35,25 @@ export default function Notification({params}: {params: {processID: string}}) {
     flightCost: 0,
     baggageCost: 0,
     otherCosts: 0,
-    totalCost: 0
+    totalCost: 0,
   }
-  const [costInfo, setCostInfo] =  useState<CostsData>(defaultCostsData)
+  console.log(params.processID)
+  const [costInfo, setCostInfo] = useState<CostsData>(defaultCostsData)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`https://backendoficial-presencial-pagosb.onrender.com/flights/pricebyid?id=${params?.processID}`)
-      const data= await res.json()
+      const res = await fetch(
+        `https://backendoficial-presencial-pagosb.onrender.com/flights/pricebyid?id=${params?.processID}`
+      )
+      const data = await res.json()
       setCostInfo(data as CostsData)
+      setIsLoading(false)
     }
     fetchData()
   }, [])
 
   console.log(costInfo)
-  
+
   let responseStatus = true
   return (
     <>
@@ -70,107 +70,112 @@ export default function Notification({params}: {params: {processID: string}}) {
             height: "95%",
           }}
         >
-          <Image src={checked} alt="Checked" width={50} height={50} />
-          <Typography component="h1" variant="h5" color={"black"} fontSize={30} sx={{ marginBottom: "30px" }}>
-            Transacción aprobada
-          </Typography>
-
-          <Grid
-            container
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "60%",
-            }}
-            spacing={3}
-          >
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
-                Comprobante N°:
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
-                48546952
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
-                Fecha:
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
-                {getCurrentDate()}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
-                Valor pagado:
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
-                {costInfo.totalCost}$
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
-                Descripción:
-              </Typography>
-            </Grid>
-
-            <Grid container spacing={1} sx={{ width: "90%", marginTop: "15px" }}>
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
-                  Valor de los asientos:
+          {isLoading ? (
+            <p> ...Cargando </p>
+          ) : (
+            <>
+                <Image src={checked} alt="Checked" width={50} height={50} />
+                <Typography component="h1" variant="h5" color={"black"} fontSize={30} sx={{ marginBottom: "30px" }}>
+                  Transacción aprobada
                 </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14}>
-                  {costInfo.seatCost}$
-                </Typography>
-              </Grid>
-
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
-                  Valor de los equipajes:
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14}>
-                 {costInfo.baggageCost}$
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
-                  Impuestos adicionales:
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography component="h5" color={"black"} fontSize={14}>
-                  50.00$
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={6}>
-              <button
-                className="relative m-0 my-4 box-border inline-flex w-full min-w-[64px] cursor-pointer items-center justify-center rounded border-0 bg-[#2196F3] px-4 py-1.5 align-middle font-medium text-[15] text-[white] no-underline hover:bg-[#0e6ecd]"
-                onClick={() => {}}
+              <Grid
+                container
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "60%",
+                }}
+                spacing={3}
               >
-                ENVIAR COMPROBANTE
-              </button>
-            </Grid>
-            <Grid item xs={6}>
-              <button
-                className="relative m-0 my-4 box-border inline-flex w-full min-w-[64px] cursor-pointer items-center justify-center rounded border-0 bg-[#2196F3] px-4 py-1.5 align-middle font-medium text-[15] text-[white] no-underline hover:bg-[#0e6ecd]"
-                onClick={() => {}}
-              >
-                FINALIZAR
-              </button>
-            </Grid>
-          </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
+                    Comprobante N°:
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
+                    48546952
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
+                    Fecha:
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
+                    {getCurrentDate()}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
+                    Valor pagado:
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} align="right">
+                    {costInfo.totalCost}$
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography component="h1" variant="h5" color={"black"} fontSize={15} fontWeight={"bold"}>
+                    Descripción:
+                  </Typography>
+                </Grid>
+
+                <Grid container spacing={1} sx={{ width: "90%", marginTop: "15px" }}>
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
+                      Valor de los asientos:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14}>
+                      {costInfo.seatCost}$
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
+                      Valor de los equipajes:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14}>
+                      {costInfo.baggageCost}$
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14} fontWeight={"bold"}>
+                      Impuestos adicionales:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography component="h5" color={"black"} fontSize={14}>
+                      50.00$
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <button
+                    className="relative m-0 my-4 box-border inline-flex w-full min-w-[64px] cursor-pointer items-center justify-center rounded border-0 bg-[#2196F3] px-4 py-1.5 align-middle font-medium text-[15] text-[white] no-underline hover:bg-[#0e6ecd]"
+                    onClick={() => {}}
+                  >
+                    ENVIAR COMPROBANTE
+                  </button>
+                </Grid>
+                <Grid item xs={6}>
+                  <button
+                    className="relative m-0 my-4 box-border inline-flex w-full min-w-[64px] cursor-pointer items-center justify-center rounded border-0 bg-[#2196F3] px-4 py-1.5 align-middle font-medium text-[15] text-[white] no-underline hover:bg-[#0e6ecd]"
+                    onClick={() => {}}
+                  >
+                    FINALIZAR
+                  </button>
+                </Grid>
+              </Grid>
+            </>
+          )}
         </Container>
       ) : (
         <Container
