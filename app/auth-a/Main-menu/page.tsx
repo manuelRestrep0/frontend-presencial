a853c1036e1eb52ae52a52048ce03dd2b9db5c49
Sebastian Pelaez <sebastianpz122@gmail.com>
2024-05-19 20:58:15 -0500

    Paginas terminadas con conexion al login y el signup

--- app/auth-a/Main-menu/page.tsx
+++ app/auth-a/Main-menu/page.tsx
@@ -1,7 +1,40 @@
-import React from "react"
+"use client"
+import Link from "next/link"
+import React, { useState } from "react"
 import { IconAirplane } from "components/components-Auth-a/IconAirplane"
 
 const Header: React.FC = () => {
+  const [validToken] = useState(false)
+
+  const displaySessionState = () => {
+    if (validToken) {
+      return (
+        <>
+          <button className="bg-neutral-0 rounded border-2 border-blue-500 px-6 py-3 text-blue-500">
+            Administrar roles
+          </button>
+          <div className="flex flex-col items-center">
+            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 text-neutral-500">
+              <span className="text-xl">👤</span>
+            </div>
+            <span className="text-neutral-500">User Name</span>
+          </div>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <button className="rounded bg-blue-500 px-6 py-3 text-neutral-50">
+            <Link href={"Login"}>Iniciar sesión</Link>
+          </button>
+          <button className="bg-neutral-0 rounded border-2 border-blue-500 px-6 py-3 text-blue-500">
+            <Link href={"signup"}>Registrarse</Link>
+          </button>
+        </>
+      )
+    }
+  }
+
   return (
     <header className="mb-4 flex items-center justify-between border-b border-gray-300 bg-neutral-50 p-4 text-white">
       <div className="flex items-center">
@@ -9,16 +42,13 @@ const Header: React.FC = () => {
           <div className="w-10">
             <IconAirplane />
           </div>
-          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500 ">Reservar</button>
-          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500 ">Tus vuelos</button>
-          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500 ">Pagos</button>
-          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500 ">Check in</button>
+          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500">Reservar</button>
+          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500">Tus vuelos</button>
+          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500">Pagos</button>
+          <button className="bg-white-600 rounded px-6 py-3 text-neutral-500">Check in</button>
         </div>
       </div>
-      <div className="space-x-4">
-        <button className="rounded bg-blue-500 px-6 py-3 text-neutral-50">Iniciar sesión</button>
-        <button className="bg-neutral-0 rounded border-2 border-blue-500 px-6 py-3 text-blue-500">Registrarse</button>
-      </div>
+      <div className="flex items-center space-x-4">{displaySessionState()}</div>
     </header>
   )
 }
