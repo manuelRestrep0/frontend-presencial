1fb9f0602e228e7773755ef41e7a3feb0bb89f7b
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-05-22 13:53:53 -0500

    Add history api conection

--- app/reservas-a/components/organisms/ReservationsTables.tsx
+++ app/reservas-a/components/organisms/ReservationsTables.tsx
@@ -5,10 +5,20 @@ import TableCell from "@mui/material/TableCell"
 import TableContainer from "@mui/material/TableContainer"
 import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import TableCellContent from "../atoms/texts/TableCellContent"
+import Booking from "app/reservas-a/api/booking/interface/booking"
+import { getBookings } from "app/reservas-a/api/booking/endpoints/read"
 
 const ReservationsTable: React.FC = () => {
+  const [bookings, setBookings] = useState<Booking[]>([])
+
+  useEffect(() => {
+    getBookings()
+      .then((data) => setBookings(data))
+      .catch((error) => console.error("Error obtaining bookings", error))
+  }, [])
+
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
@@ -23,14 +33,16 @@ const ReservationsTable: React.FC = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell align="center">Reserva #0000</TableCell>
-            <TableCell align="center"></TableCell>
-            <TableCell align="center"></TableCell>
-            <TableCell align="center"></TableCell>
-            <TableCell align="center"></TableCell>
-            <TableCell align="center">SIN PAGAR</TableCell>
-          </TableRow>
+          {bookings.map((booking) => (
+            <TableRow key={booking.bookingId}>
+              <TableCell align="center">{booking.bookingId}</TableCell>
+              <TableCell align="center">{booking.flightId}</TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">{booking.booking_status}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
